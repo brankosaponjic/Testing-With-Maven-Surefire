d720d5c1fb697a052b8e589eea11b62fbb0aefc0
Branko <brankosaponjic76@gmail.com>
2021-03-28 23:09:32 +0200

    Running Integration Tests with Maven Failsafe.

---
+++ src/test/java/brankosaponjic/JavaHelloMavenIT.java
@@ -0,0 +1,10 @@
+package brankosaponjic;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloMavenIT {
+    @Test
+    void myFauxIntegrationTest() {
+        System.out.println("My IT Ran");
+    }
+}
